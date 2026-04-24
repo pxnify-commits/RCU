6e234de42678b54dacc4116c948d44a7b1f5497c
pxnify-commits <pxnify@gmail.com>
2026-04-24 15:18:54 +0200

    Update Core.lua

--- Core.lua
+++ Core.lua
@@ -1,25 +1,16 @@
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
--- Remote Pfade (Zentralisiert)
-local RS = game:GetService("ReplicatedStorage")
-local BotStr = "jag k\195\164nner en bot, hon heter anna, anna heter hon"
-local ServicePath = RS:WaitForChild("Packages"):WaitForChild("Knit"):WaitForChild("Services"):WaitForChild(BotStr)
-
-getgenv().FarmHub.RE = ServicePath:WaitForChild("RE"):WaitForChild(BotStr)
-getgenv().FarmHub.RF = ServicePath:WaitForChild("RF"):WaitForChild(BotStr)
-
--- Fenster Erstellung
 getgenv().FarmHub.Window = Rayfield:CreateWindow({
     Name = "RCU Modular Hub",
-    LoadingTitle = "Synchronisiere Module...",
+    LoadingTitle = "GitHub Sync...",
     ConfigurationSaving = { Enabled = false },
     KeySystem = false
 })
 
--- Tabs global verfügbar machen
+-- Nur die leeren Tabs erstellen
 getgenv().FarmHub.Tabs.Eggs = getgenv().FarmHub.Window:CreateTab("🥚 Eggs")
 getgenv().FarmHub.Tabs.Farms = getgenv().FarmHub.Window:CreateTab("🌾 Auto Farms")
 getgenv().FarmHub.Tabs.Machines = getgenv().FarmHub.Window:CreateTab("⚙️ Machines")
 getgenv().FarmHub.Tabs.Minigames = getgenv().FarmHub.Window:CreateTab("🎮 Minigames")
 
-print("✅ Core geladen")
+print("✅ Core UI Ready")
